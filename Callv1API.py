6516c0fe91951769f092eecab07387671ff8d710
NickdaFlavin <73003135+NickdaFlavin@users.noreply.github.com>
2023-12-05 12:34:31 -0800

    Create V1 Get call

--- Callv1API.py
+++ Callv1API.py
@@ -1,9 +1,25 @@
 import requests
+import os
 
 
 def AppfolioAPIV1_GET(endpoint: str):
-    print("Oh no")
-    print()
 
+    appfolio_client_ID = os.environ.get('APPFOLIO_CLIENT_ID')
+    appfolio_client_Secret = os.environ.get('APPFOLIO_CLIENT_SECRET')
+    appfolio_database = os.environ.get('APPFOLIO_DATABASE_NAME')
 
+    if appfolio_client_ID and appfolio_client_Secret and appfolio_database:
+        api_url = f"https://{appfolio_database}.appfolio.com/api/v1/reports/{endpoint}"
+    else:
+        return "ERROR: Retrieving Secrets"
+    
+    try:
+        response = requests.get(api_url, auth=(appfolio_client_ID, appfolio_client_Secret))
+        if response.status_code == 200:
+            return response.text
+        else:
+            return f"ERROR: Invaild Response: {response.status_code}"
 
+    except requests.RequestException as e:
+        return f"ERROR: Request Failed - {e}"
+    
