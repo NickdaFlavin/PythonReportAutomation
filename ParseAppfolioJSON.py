99189055624ce3740b598d99085311bae8141d84
NickdaFlavin <73003135+NickdaFlavin@users.noreply.github.com>
2023-12-05 13:05:32 -0800

    Test Parsing

--- ParseAppfolioJSON.py
+++ ParseAppfolioJSON.py
@@ -1,4 +1,10 @@
-
+import json
 
 def ParseAppfolioV1JSON(jsonstring: str):
+    jsonstring.replace("'", '"')
+    jsonstring.replace("None", '"NULL"')
+    parsed_data = json.loads(jsonstring)
+    return parsed_data
     
+
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from Callv1API import AppfolioAPIV1_GET
+from ParseAppfolioJSON import ParseAppfolioV1JSON
 
-print(AppfolioAPIV1_GET("balance_sheet.json"))
+print(type(ParseAppfolioV1JSON(AppfolioAPIV1_GET("balance_sheet.json"))))
+print(ParseAppfolioV1JSON(AppfolioAPIV1_GET("balance_sheet.json")))
