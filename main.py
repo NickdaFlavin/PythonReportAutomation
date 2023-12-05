cb7430c1a586baa429492ea8c0fd362906d544f6
NickdaFlavin <73003135+NickdaFlavin@users.noreply.github.com>
2023-12-05 13:23:27 -0800

    Print cash flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Callv1API import AppfolioAPIV1_GET
 from ParseAppfolioJSON import ParseAppfolioV1JSON
-
+print(AppfolioAPIV1_GET("tweleve_month_cash_flow.json"))
 
 print(ParseAppfolioV1JSON(AppfolioAPIV1_GET("tweleve_month_cash_flow.json")))
