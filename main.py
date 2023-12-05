7a779faea45d910fd66128fda028191f140d6417
NickdaFlavin <73003135+NickdaFlavin@users.noreply.github.com>
2023-12-05 13:24:56 -0800

    Fixed typo

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Callv1API import AppfolioAPIV1_GET
 from ParseAppfolioJSON import ParseAppfolioV1JSON
 print(AppfolioAPIV1_GET("tweleve_month_cash_flow.json"))
 
-print(ParseAppfolioV1JSON(AppfolioAPIV1_GET("tweleve_month_cash_flow.json")))
+print(ParseAppfolioV1JSON(AppfolioAPIV1_GET("twelve_month_cash_flow.json")))
