0250166c1999807c755d402c35214d6bcf165a3e
NickdaFlavin <73003135+NickdaFlavin@users.noreply.github.com>
2023-12-05 17:30:01 -0800

    Removed PyQt

--- CreateUI.py
+++ CreateUI.py
@@ -2,19 +2,3 @@
 import sys
 import os 
 
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-
-class MainWindow(QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
