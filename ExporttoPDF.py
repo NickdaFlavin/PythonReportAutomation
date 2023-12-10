72af9243d51d2a8d8e061a0b9f19d866177e1f16
NickdaFlavin <everything1011@icloud.com>
2023-12-10 01:11:48 -0800

    Created PDF Printer

--- ExporttoPDF.py
+++ ExporttoPDF.py
@@ -5,11 +5,28 @@ import v1.Createv1Endpoint as v1
 import v2.Createv2Endpoint as v2
 
 import pandas as pd
+import json
+from fpdf import FPDF
 
 
 endpoint, json_data = v2.CashFlow("01/01/23", "12/31/23", property_ids=[79])
 
-print(AppfolioAPIv2_POST(endpoint, json_data))
+
+api_data = json.loads(AppfolioAPIv2_POST(endpoint, json_data))
 
 
-pd.DataFrame
+df = pd.DataFrame(api_data)
+columns = ['AccountCode'] + [col for col in df.columns if 'Slice' in col]
+new_df = df[columns]
+
+pdf = FPDF('L')
+
+pdf.add_page()
+
+pdf.set_font('Times', '', 12)
+
+for index, row in new_df.iterrows():
+    for i, col in enumerate(row):
+        pdf.cell(20, 10, txt=str(col).encode().decode('latin-1', 'strict'), ln=True if i == len(row)-1 else 0)
+
+pdf.output('ouput.pdf', 'F')
